e4b5c7d3345e32d5188da40545d3d9f55d261560
PratikshaGhanawat <94125522+PratikshaGhanawat@users.noreply.github.com>
2023-12-22 21:56:52 +0530

    Add files via upload

---
+++ StateManagementDemo.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StateManagementDemo.aspx.cs" Inherits="Question4validation.StateManagementDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>State Management Demo</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>User Preferences</h2>
+            <label for="txtUsername">Username:</label>
+            <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            <br />
+
+            <label for="ddlTheme">Preferred Theme:</label>
+            <asp:DropDownList ID="ddlTheme" runat="server">
+                <asp:ListItem Text="Light" Value="light" />
+                <asp:ListItem Text="Dark" Value="dark" />
+            </asp:DropDownList>
+            <br />
+
+            <asp:Button ID="btnSavePreferences" runat="server" Text="Save Preferences" OnClick="btnSavePreferences_Click" />
+            <asp:Button ID="btnClearSession" runat="server" Text="Clear Session" OnClick="btnClearSession_Click" />
+        </div>
+
+        <div>
+            <h2>View State Information</h2>
+            <asp:Label ID="lblViewStateInfo" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StateManagementDemo.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Question4validation
+{
+    public partial class StateManagementDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check if there is data in the ViewState
+                if (ViewState["UserInfo"] != null)
+                {
+                    string userInfo = ViewState["UserInfo"].ToString();
+                    lblViewStateInfo.Text = $"View State Info: {userInfo}";
+                }
+            }
+        }
+
+        protected void btnSavePreferences_Click(object sender, EventArgs e)
+        {
+            // Store user preferences in Session State
+            Session["Username"] = txtUsername.Text;
+            Session["Theme"] = ddlTheme.SelectedValue;
+
+            // Store a piece of information in View State
+            ViewState["UserInfo"] = $"Username: {txtUsername.Text}, Theme: {ddlTheme.SelectedValue}";
+
+            // Display user preferences
+            lblViewStateInfo.Text = $"View State Info: {ViewState["UserInfo"]}";
+        }
+
+        protected void btnClearSession_Click(object sender, EventArgs e)
+        {
+            // Clear Session State data
+            Session.Clear();
+            lblViewStateInfo.Text = "View State Info: (Cleared)";
+        }
+    }
+}
